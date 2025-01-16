4913a165b34a4c4fef7efc039c444fe2c3856036
tuandaynek <letuanminhlqd@gmail.com>
2025-01-16 15:43:03 +0700

    test upload nodejs project

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const hostname = 'localhost';
+const port = 100;
+
+//morgan (http logger)
+app.use(morgan("combined"));
+
+//handlebars (template engine)
+app.engine('hbs', engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+app.get('/', (req, res) => {
+  res.render('products');
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
